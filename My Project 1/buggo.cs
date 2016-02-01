8d42723bd26af12b58f722a13ba72616a5c7d873
p <p>
2016-02-01 08:36:35 +0545

    abc

--- My Project 1/buggo.cs
+++ My Project 1/buggo.cs
@@ -3,27 +3,32 @@ if (VersionControl.createRepo(project))
     this.createLocalFile();
     Console.WriteLine("Local File Created");
     panel2.UseWaitCursor = true;
+    
+    //commit details accessed from version control and stored in Idictionary
     IDictionary<string, string> auditDetails = VersionControl.pushFile(filename, project, details);
     panel2.UseWaitCursor = false;
-    
-    
     Console.WriteLine("Pushed to GitHub");
-    this.updateBug();
+    //to add bug
+    this.addBug();
     Console.WriteLine("Added to Bug table");
     //MessageBox.Show(auditDetails["isEmpty"]);
+    string msg = "";
+    //if commit was made
     if (auditDetails["isEmpty"] != "True")
     {
-        bugAudit["status"] = "Updated";
+        bugAudit["status"] = "Created";
         bugAudit["hash"] = auditDetails["hash"];
         bugAudit["contributor"] = auditDetails["contributor"];
         bugAudit["time"] = auditDetails["time"];
         bugAudit["details"] = details;
         bugAudit["filename"] = filename;
+        //records insert to audit
         Audit audit = new Audit();
         audit.insertAudit(bugAudit);
         Console.WriteLine("Audit Added");
+        msg = "pushed to GitHub and";
     }
     lblMessage.ForeColor = System.Drawing.Color.Green;
-    lblMessage.Text = "Success! Your bug was pushed to GitHub and added to Bugs";
+    lblMessage.Text = "Success! Your bug was " + msg + " added to Bugs";
     this.firstPage();
 }
